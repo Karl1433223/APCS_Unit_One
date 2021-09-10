abce0f122733ce9b9cca4d8d503989862fbebd90
karl.feng <Fengka@ssfs.org>
2021-09-10 11:54:12 -0400

    finished 1.6 practice

---
+++ src/DateFormat.java
@@ -0,0 +1,21 @@
+public class DateFormat {
+    Public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the current month? ");
+        String month = scan.nextLine();
+        System.out.println("what is the day of the week? ");
+        String dow = scan.nextLine();
+        System.out.println("What is the current year? ");
+        int year = scan, nextInt ();
+        System.out.println("What is the day of the month ");
+        int dayOf month = scan.nextInt();
+
+        System.out.println([])
+    }
+}
+
+
+
+
+
