fff33cef0760712133491badaaded379d7e0f514
karl.feng <Fengka@ssfs.org>
2021-09-16 08:59:37 -0400

    finished 1.6 practice

--- src/RoundNegatives.java
+++ src/RoundNegatives.java
@@ -5,14 +5,17 @@ public class RoundNegatives {
     public static void main(String[] args)
     {
         // Create a Scanner object and write a prompt for the user to enter a number
-
+         Scanner Scan = new Scanner(System.in);
         // Declare a variable of the correct type that will receive the input from the user.
-
+          System.out.println("A");
+          double A= Scan.nextDouble();
         // Declare an variable and write an expression to round the user input
+          int B=(int)(A-0.5);
+
         // ex. int roundedNum=/*expression to round a negative value*/;
 
         // Write a statement that will output the result in the form below.
         // ex. -3.21 rounded to a whole number is -3
-
+           System.out.println("-3.21 = ");
     }
-}
+    }
